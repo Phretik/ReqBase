8337018dc2c700a6cad9ec2c361912b6d786005f
Phretik <51486871+Phretik@users.noreply.github.com>
2024-03-21 12:03:02 +0000

    More test stuff

--- App/tests/conftest.py
+++ App/tests/conftest.py
@@ -2,4 +2,17 @@ import pytest
 from flask import Flask, request
 from ReqBase import auth, db, DB_NAME
 from ReqBase.models import User
-from App.ReqBase import create_app
+from App import create_app
+
+@pytest.fixture()
+def app():
+    app = create_app("sqlite://")
+
+    with app.app_context():
+        db.create_all()
+
+    yield app
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
--- App/tests/test_signup.py
+++ App/tests/test_signup.py
@@ -2,3 +2,7 @@ import pytest
 from flask import Flask, request
 from ReqBase import auth, db, DB_NAME
 from ReqBase.models import User
+
+def test_signup(client):
+    response = client.get("/sign-up")
+    assert b"<title>Sign up</title>" in response.data
