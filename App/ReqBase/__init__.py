4f225a6a3fce2b50f79244b80e63d18ae46d3804
Phretik <51486871+Phretik@users.noreply.github.com>
2024-03-21 21:18:55 +0000

    Changed db.session

--- App/ReqBase/__init__.py
+++ App/ReqBase/__init__.py
@@ -36,7 +36,7 @@ def create_app():
 
     @login_manager.user_loader
     def load_user(id):
-        return db.session.get(int(id))
+        return db.session.query(User).get(int(id))
         ##return User.query.get(int(id))     ##Loads user
 
     return app
