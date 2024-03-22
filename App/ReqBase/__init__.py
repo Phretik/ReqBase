2ac12ed35ee2a9dd89b9443e98cce089c852d833
Phretik <51486871+Phretik@users.noreply.github.com>
2024-03-22 05:27:33 +0000

    Changed URI back

--- App/ReqBase/__init__.py
+++ App/ReqBase/__init__.py
@@ -16,7 +16,7 @@ def create_app():
     
     
     app.config['SECRET_KEY'] = 'Obliviate'
-    app.config['SQLALCHEMY_DATABASE_URI'] = r'/var/data'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     
     db.init_app(app)
 
